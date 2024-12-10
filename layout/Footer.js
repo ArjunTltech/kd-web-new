3217075a673f3506c9d8122f9d21b7285f906e29
guru-das-pr <gurudaspr.tltechnologies@gmail.com>
2024-12-10 17:34:26 +0530

    added footer links

--- layout/Footer.js
+++ layout/Footer.js
@@ -139,13 +139,14 @@ const Footer1 = () => {
               <div className="footer-widget footer-text">
                 <div className="footer-logo mb-25">
                   <Link href="/">
-                    <img src="assets/images/logos/logo.png" alt="Logo" />
+                    <img src="/assets/images/logos/logo-white.png"
+                    title="Logo"
+                    className="img-fluid"
+                    style={{ height: "50px" }} />
                   </Link>
                 </div>
                 <p>
-                  We curate bespoke itineraries tailored to your preferences,
-                  ensuring every trip is seamless and enriching hidden gems
-                  beaten
+                Uncover South India&apos;s Hidden Gems &ndash; Where Every Trip Turns Into a Timeless Memory!
                 </p>
                 <div className="social-style-one mt-15">
                   <Link href="contact">
@@ -201,7 +202,7 @@ const Footer1 = () => {
       </div>
       <div className="widget-area pt-95 pb-45">
         <div className="container">
-          <div className="row row-cols-xl-5 row-cols-lg-4 row-cols-md-3 row-cols-2">
+          <div className="row row-cols-xl-4 row-cols-lg-4 row-cols-md-3 row-cols-2">
             <div
               className="col col-small"
               data-aos="fade-up"
@@ -210,23 +211,20 @@ const Footer1 = () => {
             >
               <div className="footer-widget footer-links">
                 <div className="footer-title">
-                  <h5>Services</h5>
+                  <h5>Packages</h5>
                 </div>
                 <ul className="list-style-three">
                   <li>
-                    <Link href="destination-details">Best Tour Guide</Link>
+                    <Link href="tour-grid">Honeymoon</Link>
                   </li>
                   <li>
-                    <Link href="destination-details">Tour Booking</Link>
+                    <Link href="tour-grid">Educational</Link>
                   </li>
                   <li>
-                    <Link href="destination-details">Hotel Booking</Link>
+                    <Link href="tour-grid">Traditional</Link>
                   </li>
                   <li>
-                    <Link href="destination-details">Ticket Booking</Link>
-                  </li>
-                  <li>
-                    <Link href="destination-details">Rental Services</Link>
+                    <Link href="tour-grid">Group Adventures</Link>
                   </li>
                 </ul>
               </div>
@@ -244,19 +242,16 @@ const Footer1 = () => {
                 </div>
                 <ul className="list-style-three">
                   <li>
-                    <Link href="about">About Company</Link>
+                    <Link href="about">About Us</Link>
                   </li>
                   <li>
-                    <Link href="blog">Community Blog</Link>
+                    <Link href="blog">Blogs</Link>
                   </li>
                   <li>
-                    <Link href="contact">Jobs and Careers</Link>
+                    <Link href="gellery-grid">Gallery</Link>
                   </li>
                   <li>
-                    <Link href="blog">latest News Blog</Link>
-                  </li>
-                  <li>
-                    <Link href="contact">Contact Us</Link>
+                    <Link href="faqs">Faqs</Link>
                   </li>
                 </ul>
               </div>
@@ -291,36 +286,7 @@ const Footer1 = () => {
                 </ul>
               </div>
             </div>
-            <div
-              className="col col-small"
-              data-aos="fade-up"
-              data-aos-delay={150}
-              data-aos-duration={1500}
-              data-aos-offset={50}
-            >
-              <div className="footer-widget footer-links">
-                <div className="footer-title">
-                  <h5>Categories</h5>
-                </div>
-                <ul className="list-style-three">
-                  <li>
-                    <Link href="contact">Adventure</Link>
-                  </li>
-                  <li>
-                    <Link href="contact">Hiking &amp; Trekking</Link>
-                  </li>
-                  <li>
-                    <Link href="contact">Cycling Tours</Link>
-                  </li>
-                  <li>
-                    <Link href="contact">Family Tours</Link>
-                  </li>
-                  <li>
-                    <Link href="contact">Wildlife Tours</Link>
-                  </li>
-                </ul>
-              </div>
-            </div>
+            
             <div
               className="col col-md-6 col-10 col-small"
               data-aos="fade-up"
@@ -362,7 +328,7 @@ const Footer1 = () => {
             <div className="col-lg-5">
               <div className="copyright-text text-center text-lg-start">
                 <p>
-                  @Linkopy 202/4 <a href="">Ravelo</a>, All rights reserved
+                  &copy;2024 <a href="">Kerala Drives</a>, All rights reserved
                 </p>
               </div>
             </div>
@@ -373,12 +339,6 @@ const Footer1 = () => {
                 </li>
                 <li>
                   <Link href="about">Privacy Policy</Link>
-                </li>
-                <li>
-                  <Link href="about">Legal notice</Link>
-                </li>
-                <li>
-                  <Link href="about">Accessibility</Link>
                 </li>
               </ul>
             </div>
@@ -416,12 +376,15 @@ const Footer2 = ({ insta }) => {
               <div className="footer-widget footer-text">
                 <div className="footer-logo mb-40">
                   <Link href="/">
-                    <img src="assets/images/logos/logo.png" alt="Logo" />
+                  <img src="/assets/images/logos/logo-white.png"
+                    title="Logo"
+                    className="img-fluid"
+                    style={{ height: "50px" }} />
                   </Link>
                 </div>
                 <div className="footer-map">
                   <iframe
-                    src="https://www.google.com/maps/embed?pb=!1m10!1m8!1m3!1d96777.16150026117!2d-74.00840582560909!3d40.71171357405996!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sbd!4v1706508986625!5m2!1sen!2sbd"
+                    src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15636.729487185277!2d76.8642112!3d8.5824693!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3b05be56ffffffff%3A0x1c586a8fbc63e2b4!2sKerala%20Drives%20Tours%20%26%20Travels!5e1!3m2!1sen!2sin!4v1733832142357!5m2!1sen!2sin"
                     style={{ border: 0, width: "100%" }}
                     allowFullScreen=""
                     loading="lazy"
@@ -439,20 +402,20 @@ const Footer2 = ({ insta }) => {
             >
               <div className="footer-widget footer-links ms-sm-5">
                 <div className="footer-title">
-                  <h5>Services</h5>
+                  <h5>Packages</h5>
                 </div>
                 <ul className="list-style-three">
                   <li>
-                    <Link href="destination-details">Best Tour Guide</Link>
+                    <Link href="tour-grid">Honeymoon</Link>
                   </li>
                   <li>
-                    <Link href="destination-details">Tour Booking</Link>
+                    <Link href="tour-grid">Educational</Link>
                   </li>
                   <li>
-                    <Link href="destination-details">Hotel Booking</Link>
+                    <Link href="tour-grid">Traditional</Link>
                   </li>
                   <li>
-                    <Link href="destination-details">Ticket Booking</Link>
+                    <Link href="tour-grid">Group Adventures</Link>
                   </li>
                 </ul>
               </div>
@@ -470,16 +433,16 @@ const Footer2 = ({ insta }) => {
                 </div>
                 <ul className="list-style-three">
                   <li>
-                    <Link href="about">About Company</Link>
+                    <Link href="about">About Us</Link>
                   </li>
                   <li>
-                    <Link href="blog">Community Blog</Link>
+                    <Link href="blog">Blogs</Link>
                   </li>
                   <li>
-                    <Link href="contact">Jobs and Careers</Link>
+                    <Link href="gellery-grid">Gallery</Link>
                   </li>
                   <li>
-                    <Link href="blog">latest News Blog</Link>
+                    <Link href="faqs">Faqs</Link>
                   </li>
                 </ul>
               </div>
@@ -548,8 +511,8 @@ const Footer2 = ({ insta }) => {
           <div className="row">
             <div className="col-lg-5">
               <div className="copyright-text text-center text-lg-start">
-                <p>
-                  @Linkopy 202/4 <a href="">Ravelo</a>, All rights reserved
+               <p>
+                  &copy;2024 <a href="">Kerala Drives</a>, All rights reserved
                 </p>
               </div>
             </div>
@@ -560,12 +523,6 @@ const Footer2 = ({ insta }) => {
                 </li>
                 <li>
                   <Link href="about">Privacy Policy</Link>
-                </li>
-                <li>
-                  <Link href="about">Legal notice</Link>
-                </li>
-                <li>
-                  <Link href="about">Accessibility</Link>
                 </li>
               </ul>
             </div>
