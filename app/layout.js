b79eaf471a40bff1bd0a535c77988b015fa4be9e
guru-das-pr <gurudaspr.tltechnologies@gmail.com>
2024-12-16 16:01:45 +0530

    added favicon

--- app/layout.js
+++ app/layout.js
@@ -11,10 +11,42 @@ import 'rc-slider/assets/index.css';
 
 export const metadata = {
   title: "Kerala Drives - Travel & Tour Booking",
-  description:
-    "Kerala Drives",
-};
+  description: "Explore South India with Kerala Drives, your trusted travel partner.",
 
+  icons: {
+    icon: "/assets/images/logos/favicon.png",
+    apple: "/assets/images/logos/apple-touch-icon.png",
+  },
+
+  openGraph: {
+    title: "Kerala Drives - Travel & Tour Booking",
+    description: "Explore South India with Kerala Drives, your trusted travel partner.",
+    url: "https://www.keraladrives.com",
+    siteName: "Kerala Drives",
+    images: [
+      {
+        url: "/assets/images/logos/kdlogo-black.png",
+        width: 1200,
+        height: 630,
+        alt: "Kerala Drives Open Graph Image",
+      },
+    ],
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Kerala Drives - Travel & Tour Booking",
+    description: "Explore South India with Kerala Drives, your trusted travel partner.",
+    images: ["/assets/images/logos/kdlogo-black.png"],
+  },
+
+  robots: {
+    index: true,
+    follow: true,
+  },
+
+  metadataBase: new URL("https://www.keraladrives.com"),
+};
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
