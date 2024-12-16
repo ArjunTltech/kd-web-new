fad150257af0fd34f5b32f5b3575870337fba28e
guru-das-pr <gurudaspr.tltechnologies@gmail.com>
2024-12-16 16:06:41 +0530

    added og image

--- app/layout.js
+++ app/layout.js
@@ -25,7 +25,7 @@ export const metadata = {
     siteName: "Kerala Drives",
     images: [
       {
-        url: "/assets/images/logos/kdlogo-black.png",
+        url: "https://opengraph.b-cdn.net/production/images/f5f7de32-23a4-44f0-8f5c-a9383076c771.png?token=g1oyiqLNnfFdP_rUkGCS_TinmsysQfDj1d-zuqZ4QMQ&height=333&width=1200&expires=33270344912",
         width: 1200,
         height: 630,
         alt: "Kerala Drives Open Graph Image",
@@ -37,7 +37,7 @@ export const metadata = {
     card: "summary_large_image",
     title: "Kerala Drives - Travel & Tour Booking",
     description: "Explore South India with Kerala Drives, your trusted travel partner.",
-    images: ["/assets/images/logos/kdlogo-black.png"],
+    images: ["https://opengraph.b-cdn.net/production/images/f5f7de32-23a4-44f0-8f5c-a9383076c771.png?token=g1oyiqLNnfFdP_rUkGCS_TinmsysQfDj1d-zuqZ4QMQ&height=333&width=1200&expires=33270344912"],
   },
 
   robots: {
